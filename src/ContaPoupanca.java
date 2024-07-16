c7fbe39942cb5c31b55f0ea1319e881d5f462943
Wallace Queiros <wallacequeiros@hotmail.com>
2024-07-16 10:23:53 -0300

    Criação dos atributos e alguns metodos das classes ContaCorrente e ContaPoupanca

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,2 +1,15 @@
 public class ContaPoupanca {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(double valor){
+
+    }
+
+    public void depositar(double valor){
+
+    }
+
 }
